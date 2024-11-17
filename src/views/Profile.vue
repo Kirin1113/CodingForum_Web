2690392ace22f46c55454a5abf728f1c9d041f6d
BuildTools <unconfigured@null.spigotmc.org>
2024-11-17 23:19:12 +0800

    訂閱/關注徽章

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -61,9 +61,31 @@
               <ul class="list-group">
                 <li class="text-sm border-0 list-group-item ps-0">
                   <strong class="text-dark">訂閱人數:</strong> &nbsp; {{ user.subscriptions }}
+                  <span style="display: inline-block; vertical-align: middle;">
+                    <div v-if="user.subscriptions >= 32 && user.subscriptions < 62">
+                      <img src="http://localhost/Coding-platform/CodingPlatform_api/public/uploads/badge/badge11.png" style="width: 32px; height: 35px; margin-left: 5px; margin-bottom: 10px;">
+                    </div>
+                    <div v-if="user.subscriptions >= 62 && user.subscriptions < 123">
+                      <img src="http://localhost/Coding-platform/CodingPlatform_api/public/uploads/badge/badge12.png" style="width: 32px; height: 35px; margin-left: 5px; margin-bottom: 10px;">
+                    </div>
+                    <div v-if="user.subscriptions >= 123">
+                      <img src="http://localhost/Coding-platform/CodingPlatform_api/public/uploads/badge/badge13.png" style="width: 32px; height: 35px; margin-left: 5px; margin-bottom: 10px;">
+                    </div>
+                  </span>
                 </li>
                 <li class="text-sm border-0 list-group-item ps-0">
                   <strong class="text-dark">關注人數:</strong> &nbsp; {{ user.follows }}
+                  <span style="display: inline-block; vertical-align: middle;">
+                    <div v-if="user.follows >= 32 && user.follows < 62">
+                      <img src="http://localhost/Coding-platform/CodingPlatform_api/public/uploads/badge/badge21.png" style="width: 30px; height: 30px; margin-left: 5px; margin-bottom: 5px;">
+                    </div>
+                    <div v-if="user.follows >= 62 && user.follows < 123">
+                      <img src="http://localhost/Coding-platform/CodingPlatform_api/public/uploads/badge/badge22.png" style="width: 30px; height: 30px; margin-left: 5px; margin-bottom: 5px;">
+                    </div>
+                    <div v-if="user.follows >= 123">
+                      <img src="http://localhost/Coding-platform/CodingPlatform_api/public/uploads/badge/badge23.png" style="width: 30px; height: 30px; margin-left: 5px; margin-bottom: 5px;">
+                    </div>
+                  </span>
                 </li>
                 <li class="text-sm border-0 list-group-item ps-0">
                   <strong class="text-dark">電子郵件:</strong> &nbsp; {{ user.email }}
